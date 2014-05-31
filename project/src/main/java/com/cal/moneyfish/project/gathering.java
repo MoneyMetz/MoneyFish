9c9abad8f7368cd6675e5c36417f7ccf08665022
jmetzler <jjmetzler@Jons-MacBook-Pro.local>
2014-05-31 01:07:23 -0500

    added parse lib, also finished splash screen

--- project/src/main/java/com/cal/moneyfish/project/gathering.java
+++ project/src/main/java/com/cal/moneyfish/project/gathering.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
 /**
  * Created by Jon on 5/16/2014.
  */
@@ -13,7 +17,11 @@ public class gathering extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "HA8hBtbS72Ss8BaVVVl3I8aYyapKaMpHkiit6KWG", "vyezwecy91RFjJDk6BGBdnrqgrCEygbQ5Bcx73vV");
         setContentView(R.layout.gathering);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
 
