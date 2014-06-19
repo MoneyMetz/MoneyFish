acf11c6e0320c5e81a4cfc55a717ec891ea3b411
Jonathan Metzler <jmetzler@compcc.com>
2014-06-19 16:03:13 -0500

    push

--- project/src/main/java/com/cal/moneyfish/project/splashscreen.java
+++ project/src/main/java/com/cal/moneyfish/project/splashscreen.java
@@ -26,9 +26,6 @@ import android.view.View;
 
 public class splashscreen extends Activity {
 
-    PaintView pV;
-    RelativeLayout rlsplash;
-
     private final int SPLASH_DISPLAY_LENGHT = 1400;
 
     /** Called when the activity is first created. */
@@ -36,13 +33,6 @@ public class splashscreen extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.splashscreen);
-
-        pV=new PaintView(this);
-        rlsplash=(RelativeLayout)findViewById(R.id.rlsplash);
-        pV=new PaintView(this);
-        rlsplash.addView(pV);
-
-
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
@@ -56,42 +46,6 @@ public class splashscreen extends Activity {
             }
         }, SPLASH_DISPLAY_LENGHT);
     }
-
-
-
-
-
-    public class PaintView extends View {
-        private static final String Text = "MoneyFish";
-        private Path myArc;
-        private Paint mPaintText;
-
-        public PaintView(Context context) {
-            super(context);
-            //create Path object
-            myArc = new Path();
-            //create RectF Object
-            RectF oval = new RectF(50,100,200,250);
-            //add Arc in Path with start angle -180 and sweep angle 200
-            myArc.addArc(oval, -180, 200);
-            //create paint object
-            mPaintText = new Paint(Paint.ANTI_ALIAS_FLAG);
-            //set style
-            mPaintText.setStyle(Paint.Style.FILL_AND_STROKE);
-            //set color
-            mPaintText.setColor(Color.BLACK);
-            //set text Size
-            mPaintText.setTextSize(50f);
-
-        }
-        @Override
-        protected void onDraw(Canvas canvas) {
-            //Draw Text on Canvas
-            canvas.drawTextOnPath(Text, myArc, 0, 20, mPaintText);
-            invalidate();
-        }
-    }
-
 
 }
 
